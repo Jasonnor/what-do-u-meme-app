0f0ddd9048e9de4b8b5d523dc20efbab8292f299
Jeff King <goan15910@gmail.com>
2019-06-04 20:14:31 +0800

    frontend

--- app/search_by_text.go
+++ app/search_by_text.go
@@ -6,9 +6,32 @@ import (
 	"net/http"
 )
 
+type searchOutputItem struct {
+	ImageUrl string `json:"image_url"`
+	Title    string `json:"title"`
+	ItemUrl  string `json:"item_url"`
+}
+
 func SearchByTextMock(writer http.ResponseWriter, request *http.Request) {
-	jsonString, _ := json.Marshal("mock search results")
+	writer.Header().Set("Access-Control-Allow-Origin", "*")
+	const numOfResults = 9
+	jsonMockList := createJsonMockList(numOfResults)
+	jsonString, _ := json.Marshal(jsonMockList)
 	if _, err := writer.Write(jsonString); err != nil {
 		fmt.Println(err.Error())
 	}
+}
+
+func createJsonMockList(numOfResults int) []searchOutputItem {
+	jsonMockList := make([]searchOutputItem, numOfResults)
+	for i := 0; i < numOfResults; i++ {
+		jsonMock := searchOutputItem{
+			ImageUrl: "https://i.imgflip.com/32hp9x.jpg",
+			Title:    "Batman Slapping Robin",
+			ItemUrl:  "https://imgflip.com/meme/Batman-Slapping-Robin",
+		}
+		jsonMockList[i] = jsonMock
+	}
+
+	return jsonMockList
 }
--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	"html/template"
 	"net/http"
 	"what-do-u-meme-app/app"
 )
 
+func mainPage(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseFiles("template/index.html"))
+	tmpl.Execute(w, nil)
+}
+
 func main() {
 	// add func to handle url request
+	http.HandleFunc("/", mainPage)
 	http.HandleFunc("/mock/search_by_text", app.SearchByTextMock)
 	http.HandleFunc("/mock/get_trending", app.GetTrendingMock)
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
 	// listen and serve
 	if err := http.ListenAndServe(":8080", nil); err != nil {
